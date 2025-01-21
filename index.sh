89674af7745ba65ef6fa783e510bab5bd0380db7
Sean G <seangraham006@gmail.com>
2025-01-21 19:09:22 +0000

    final touches

--- index.sh
+++ index.sh
@@ -5,12 +5,17 @@ source './utils/add_contact.sh'
 source './utils/display_contacts.sh'
 source './utils/update_contact.sh'
 source './utils/delete_contact.sh'
+source './utils/view_messages.sh'
 
 ! [ -f "contacts" ] && touch contacts
+cat "introart"
+echo -e "\n---- Welcome back to Shellegram!! ----\n"
 
 user="You,007"
 
 while true; do
+	view_messages "$user"
+
         echo -e "\n1 View Messages\n2 Text Contact\n3 Display Contacts\n4 Search for Contact\n5 Add Contact\n6 Update user\n7 Delete Contact \n8 Exit"
         read -p "Enter your choice: " ch
         case $ch in
--- utils/view_messages.sh
+++ utils/view_messages.sh
@@ -2,8 +2,39 @@
 
 folder="./chats"
 
-view_messages() {
-	ls -t "$folder" | head -n 5
-}
+LIGHT_BLUE="\033[94m"   # Light blue for user messages
+GREEN="\033[92m"        # Green for contact messages
+RESET="\033[0m"         # Reset color
 
-view_messages
+view_messages() {
+	user="$1"
+	username=$(echo "$user" | awk -F, '{print $1}')
+        usernumber=$(echo "$user" | awk -F, '{print $2}')
+        [ -z "$username" ] && user_identifier="$usernumber" || user_identifier="$username"
+	echo "ui: $user_identifier"
+
+	ls -t "$folder" | head -n 5 | while read -r file; do
+		contact_info=$(grep ",$file$" "contacts")
+		
+		name=$(echo "$contact_info" | awk -F, '{print $1}')
+		[ -z "$name" ] && identifier="$file" || identifier="$name"
+		echo -e "\nChat with $identifier"
+		
+		last_two_lines=$(tail -n 2 "$folder/$file")
+
+		echo "$last_two_lines" | while read -r line; do
+			case "$line" in
+				1:*) sender="$user_identifier"; indent="false"; message="$(echo "$line" | awk -F: '{print $2}')" ;;
+				2:*) sender="$identifier"; indent="true"; message="$(echo "$line" | awk -F: '{print $2}')" ;;
+				*) continue ;;
+			esac
+		
+			if [[ "$indent" == "true" ]]; then
+			    echo -e "$GREEN$sender: $message$RESET"
+			else
+			    echo -e "$LIGHT_BLUE$sender: $message$RESET"
+			fi
+		done
+	done
+
+}
